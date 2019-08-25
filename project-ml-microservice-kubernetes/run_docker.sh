f21c7f7de6720cf8bbdac7adca2367dc654d75e2
Walter Thornton <wfthornton@gmail.com>
2019-08-25 16:23:09 -0700

    filled in run docker

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -2,11 +2,11 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
 # Build image and add a descriptive tag
+docker build --tag=theapp .
 
-# Step 2: 
 # List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -p 80:80 theapp
